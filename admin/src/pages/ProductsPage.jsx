739e23bffcd725b22b0dd84ff9ac19cde23435a9
kubraacelik <kkbra.celik92@gmail.com>
2025-01-15 15:03:01 +0300

    updated Header title

--- admin/src/pages/ProductsPage.jsx
+++ admin/src/pages/ProductsPage.jsx
@@ -9,7 +9,7 @@ import ProductsTable from "../components/product/ProductsTable";
 const ProductsPage = () => {
   return (
     <div className="flex-1 overflow-auto relative z-10">
-      <Header title="Overview" />
+      <Header title="Products" />
 
       <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
         {/* {STATS} */}
